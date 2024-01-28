a4521afeec06f65e142319744b6bef8f0707b97c
mRezaMustofa <reza.mustofa@ist.id>
2024-01-28 18:08:38 +0700

    add

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,104 @@
 import { feathers } from '@feathersjs/feathers'
-import { koa, rest, bodyParser, errorHandler, serveStatic } from '@feathersjs/koa'
+import { koa, rest, bodyParser, errorHandler, serveStatic,cors } from '@feathersjs/koa'
 import socketio from '@feathersjs/socketio'
+require('dotenv').config();
 
-// This is the interface for the message data
+const knex = require('knex')({
+    client: 'mysql',
+    connection: process.env.DATABASE_URL
+  });
+
+
+interface User {
+  id?: number;
+  name: string;
+}
+
+
+class UserService {
+  users: User[] = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane' },
+  ];
+
+  async find() {
+    let buku = await knex('Users');
+
+    app.service('messages').create({
+      text: 'Success get users'
+    })
+    return buku;
+  }
+}
+
 interface Message {
   id?: number
   text: string
 }
 
-// A messages service that allows us to create new
-// and return all existing messages
+
 class MessageService {
   messages: Message[] = []
 
   async find() {
-    // Just return all our messages
+  
     return this.messages
   }
 
   async create(data: Pick<Message, 'text'>) {
-    // The new message is the data text with a unique identifier added
-    // using the messages length since it changes whenever we add one
+    console.log(data,this.messages)
     const message: Message = {
       id: this.messages.length,
       text: data.text
     }
 
-    // Add new message to the list
+
     this.messages.push(message)
 
     return message
   }
 }
 
-// This tells TypeScript what services we are registering
-type ServiceTypes = {
-  messages: MessageService
-}
 
-// Creates an KoaJS compatible Feathers application
+type ServiceTypes = {
+  messages: MessageService;
+  users: UserService; 
+};
+
+
 const app = koa<ServiceTypes>(feathers())
 
-// Use the current folder for static file hosting
+app.use(cors())
 app.use(serveStatic('.'))
-// Register the error handle
 app.use(errorHandler())
-// Parse JSON request bodies
 app.use(bodyParser())
-
-// Register REST service handler
 app.configure(rest())
-// Configure Socket.io real-time APIs
-app.configure(socketio())
+
+app.configure(
+  socketio({
+    cors: {
+      origin: "*"
+    }
+  }))
+  
 // Register our messages service
 app.use('messages', new MessageService())
+app.use('users', new UserService()); 
 
-// Add any new real-time connection to the `everybody` channel
-app.on('connection', (connection) => app.channel('everybody').join(connection))
-// Publish all events to the `everybody` channel
+app.on('connection', (connection) =>{ 
+app.channel('everybody').join(connection)})
 app.publish((_data) => app.channel('everybody'))
 
-// Start the server
-app
-  .listen(3030)
+
+
+app.listen(3030)
   .then(() => console.log('Feathers server listening on localhost:3030'))
 
-// For good measure let's create a message
-// So our API doesn't look so empty
 app.service('messages').create({
   text: 'Hello world from the server'
 })
+
+app.service('messages').on('created', (message: Message) => {
+  console.log('New message received:', message);
+});
+
